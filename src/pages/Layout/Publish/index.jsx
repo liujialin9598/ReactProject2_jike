bc39c2c21307868e11ef12ad0f009d880eaa8607
liuJalo <mrliujalo@foxmail.com>
2024-01-31 19:41:45 +0800

    频道列表的获取渲染

--- src/pages/Layout/Publish/index.jsx
+++ src/pages/Layout/Publish/index.jsx
@@ -14,10 +14,24 @@ import { Link } from "react-router-dom";
 import "./index.scss";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
+import { useEffect, useState } from "react";
+import { getChannelAPI } from "@/apis/article";
 
 const { Option } = Select;
 
 const Publish = () => {
+  //获取频道列表
+  const [channelList, setChannelList] = useState([]);
+  useEffect(() => {
+    //1. 封装函数 ,在函数体内调用接口
+    const getChannelList = async () => {
+      const res = await getChannelAPI();
+      setChannelList(res.data.channels);
+    };
+    //2. 调用函数
+    getChannelList();
+  }, []);
+
   return (
     <div className="publish">
       <Card
@@ -48,7 +62,11 @@ const Publish = () => {
             rules={[{ required: true, message: "Please select channel" }]}
           >
             <Select placeholder="Please select channel" style={{ width: 400 }}>
-              <Option value={0}>Recommand</Option>
+              {channelList.map((items, index) => (
+                <Option value={items.id} key={items.id}>
+                  {index}. {items.name}
+                </Option>
+              ))}
             </Select>
           </Form.Item>
           <Form.Item

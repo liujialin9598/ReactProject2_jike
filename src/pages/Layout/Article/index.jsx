da00f9fb0238ed1251ac4467937a01171cc48e36
liuJalo <mrliujalo@foxmail.com>
2024-02-01 10:06:40 +0800

    文章列表-功能介绍和搭建结构

--- src/pages/Layout/Article/index.jsx
+++ src/pages/Layout/Article/index.jsx
@@ -1,9 +1,146 @@
-import React from 'react'
+import { Link } from "react-router-dom";
+import {
+  Card,
+  Breadcrumb,
+  Form,
+  Button,
+  Radio,
+  DatePicker,
+  Select,
+} from "antd";
+// 引入date-picker汉化包
+// import locale from "antd/es/date-picker/locale/zh_CN";
 
-export default function Article() {
+// 导入资源--表格区域
+import { Table, Tag, Space } from "antd";
+import { EditOutlined, DeleteOutlined } from "@ant-design/icons";
+import img404 from "@/assets/error.png";
+
+const { Option } = Select;
+const { RangePicker } = DatePicker;
+
+const Article = () => {
+  // 准备列数据
+  const columns = [
+    {
+      title: "封面",
+      dataIndex: "cover",
+      width: 120,
+      render: (cover) => {
+        return (
+          <img src={cover.images[0] || img404} width={80} height={60} alt="" />
+        );
+      },
+    },
+    {
+      title: "标题",
+      dataIndex: "title",
+      width: 220,
+    },
+    {
+      title: "状态",
+      dataIndex: "status",
+      render: (data) => <Tag color="green">审核通过</Tag>,
+    },
+    {
+      title: "发布时间",
+      dataIndex: "pubdate",
+    },
+    {
+      title: "阅读数",
+      dataIndex: "read_count",
+    },
+    {
+      title: "评论数",
+      dataIndex: "comment_count",
+    },
+    {
+      title: "点赞数",
+      dataIndex: "like_count",
+    },
+    {
+      title: "操作",
+      render: (data) => {
+        return (
+          <Space size="middle">
+            <Button type="primary" shape="circle" icon={<EditOutlined />} />
+            <Button
+              type="primary"
+              danger
+              shape="circle"
+              icon={<DeleteOutlined />}
+            />
+          </Space>
+        );
+      },
+    },
+  ];
+  // 准备表格body数据
+  const data = [
+    {
+      id: "8218",
+      comment_count: 0,
+      cover: {
+        images: [],
+      },
+      like_count: 0,
+      pubdate: "2019-03-11 09:00:00",
+      read_count: 2,
+      status: 2,
+      title: "wkwebview离线化加载h5资源解决方案",
+    },
+  ];
   return (
     <div>
-      Article
+      <Card
+        title={
+          <Breadcrumb
+            items={[
+              { title: <Link to={"/"}>Home</Link> },
+              { title: "Article List" },
+            ]}
+          />
+        }
+        style={{ marginBottom: 20 }}
+      >
+        <Form initialValues={{ status: "" }}>
+          <Form.Item label="Status" name="status">
+            <Radio.Group>
+              <Radio value={""}>All</Radio>
+              <Radio value={0}>Draft</Radio>
+              <Radio value={2}>Verified</Radio>
+            </Radio.Group>
+          </Form.Item>
+
+          <Form.Item label="Channel" name="channel_id">
+            <Select
+              placeholder="Please select a channel"
+              defaultValue="lucy"
+              style={{ width: 120 }}
+            >
+              <Option value="jack">Jack</Option>
+              <Option value="lucy">Lucy</Option>
+            </Select>
+          </Form.Item>
+
+          <Form.Item label="Date" name="date">
+            {/* 传入locale属性 控制中文显示*/}
+            <RangePicker></RangePicker>
+          </Form.Item>
+
+          <Form.Item>
+            <Button type="primary" htmlType="submit" style={{ marginLeft: 40 }}>
+              Filter
+            </Button>
+          </Form.Item>
+        </Form>
+      </Card>
+
+      <Card title={`根据筛选条件共查询到 count 条结果：`}>
+        <Table rowKey="id" columns={columns} dataSource={data} />
+      </Card>
     </div>
-  )
-}
+  );
+};
+
+export default Article;
